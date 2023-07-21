907694ebf8e36339b1c6acbd7a6d868fa1bc9eb2
GyuChangSeong <ahdzlq12@gmail.com>
2023-07-21 12:46:11 +0900

    관리자유저 CRUD 구현 -user.xml -UserRepository 매핑

---
+++ src/main/java/com/fastcampus/admin/user/UserRepository.java
@@ -0,0 +1,20 @@
+package com.fastcampus.admin.user;
+
+import com.fastcampus.admin.report.Reason;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+import java.util.Optional;
+
+@Mapper
+public interface UserRepository {
+
+     Optional<User> findById(Long id);
+
+     List<User> findAll();
+
+     void updateById(@Param("id") Long id, @Param("role") Role role);
+
+     void updateById(@Param("id") Long id, @Param("black") boolean black, @Param("reason")Reason reason);
+}
